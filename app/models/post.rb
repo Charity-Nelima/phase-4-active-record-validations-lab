b96675640b3ea4012a5856d36b92bcae901aee8a
charity <wa.nelimacharity@gmail.com>
2022-11-28 16:47:44 +0300

    completed assign

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait_y?
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    def clickbait_y?
+        if CLICKBAIT.none? { |word| word.match title }
+            errors.add(:title, "must contain words below - Won't Believe, Secret, Top [number], Guess")
+        unless ((/Won't Believe/i.match title) || (/Secret/i.match title) || (/Top \d/i.match title) || (/Guess/i.match title)) 
+            errors.add(:title, "must be clickbait_y")
+        end
+    
+    end
+end
 end
